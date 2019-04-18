61d123d190d81a425eea1425ae68a21eb0ff005a
Softwarica College Lab <softwarica@softwarica.com>
2019-04-18 11:57:05 +0545

    more code added

--- app/src/main/java/adapter/ContactsAdapter.java
+++ app/src/main/java/adapter/ContactsAdapter.java
@@ -1,15 +1,47 @@
 package adapter;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import com.example.recycler_view.R;
 
-import de.hdodenhof.circleimageview.CircleImageView;
+import java.util.List;
 
-public class ContactsAdapter {
+import de.hdodenhof.circleimageview.CircleImageView;
+import model.Contacts;
+
+public class ContactsAdapter extends RecyclerView.Adapter<ContactsAdapter.ContactsViewHolder> {
+    Context mContext;
+    List<Contacts> contactsList;
+    public ContactsAdapter(Context mContext,List<Contacts> contactsList){
+        this.mContext = mContext;
+        this.contactsList= contactsList;
+    }
+
+    @NonNull
+    @Override
+    public ContactsViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View view = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.contact,viewGroup, false);
+
+        return new ContactsViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ContactsViewHolder contactsViewHolder, int i) {
+    Contacts contacts = contactsList.get(i);
+    ContactsViewHolder.img
+    }
+
+    @Override
+    public int getItemCount() {
+        return 0;
+    }
+
     public class ContactsViewHolder extends RecyclerView.ViewHolder{
         CircleImageView imgProfile;
         TextView tvName,tvPhone;
